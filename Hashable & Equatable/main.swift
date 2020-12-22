55b7cbe453dbd17e07adbcd600afb22d928f4bdf
Adwait Barkale <adwaitb@winjit.com>
2020-12-22 12:50:23 +0530

    Initial Commit

---
+++ Hashable & Equatable/main.swift
@@ -0,0 +1,85 @@
+//
+//  main.swift
+//  Hashable & Equatable
+//
+//  Created by Adwait Barkale on 22/12/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import Foundation
+
+//MARK:- Equatable
+
+//struct Person: Equatable {
+//    var name: String
+//}
+//
+//struct Car:Equatable {
+//    var name: String
+//    var model: String
+//    var person: Person
+//}
+
+struct Person {
+    var name: String
+}
+
+struct Car:Equatable {
+    var name: String
+    var model: String
+    var person: Person
+    
+    // This func when Person is not Equatable tell compliler what to equate
+    static func == (lhs: Car, rhs: Car) -> Bool {
+        //lhs - left side obj, rhs - right side object
+        return lhs.name == rhs.name && lhs.model == rhs.model && lhs.person.name == rhs.person.name
+    }
+}
+
+let obj1 = Car(name: "BMW", model: "C Class", person: Person(name: "joe"))
+let obj2 = Car(name: "BMW", model: "C Class", person: Person(name: "smith"))
+
+// Same only when all the values in object match each other works with Equatable protocol.
+if obj1 == obj2
+{
+    print("Same")
+} else {
+    print("Different")
+}
+
+//MARK:- Hashable
+
+struct user {
+    let name : String
+}
+
+struct userAccount: Hashable {
+    var accountIdentifier: user
+    
+    static func == (lhs: userAccount, rhs: userAccount) -> Bool {
+        return lhs.accountIdentifier.name == rhs.accountIdentifier.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(accountIdentifier.name)
+    }
+}
+
+struct Posts {
+    var text: String
+}
+
+let dictionary = [
+    userAccount(accountIdentifier: user(name: "a")) : [
+        Posts(text: "post 1"),
+        Posts(text: "post 2")
+    ], userAccount(accountIdentifier: user(name: "b")) : [
+           Posts(text: "post 3"),
+           Posts(text: "post 4")
+       ]
+]
+
+if let user2Dict = dictionary[userAccount(accountIdentifier: user(name: "a"))]
+{
+    print(user2Dict)
+}
